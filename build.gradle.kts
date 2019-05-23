47515b691596dc864cf2a1afc12da28400f4cab8
nappys-legacy <pepejohn.conquest@gmail.com>
2019-05-23 11:37:32 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/Editor.kt
@@ -0,0 +1,3 @@
+fun main() {
+    println("I'm a map editor!")
+}
